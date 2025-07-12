f4e09de19d4262ccfa2d4424cbfc31d2584b4af1
WiamRaghe <wiamraghe@gmail.com>
2025-07-12 21:39:39 +0300

    the add hidex and css

---
+++ main.js
@@ -0,0 +1,13 @@
+letx=20;
+lety=40;
+letz=20;
+letz=x+yield;
+console.log
+function changeText(){
+
+const heading=document.getElementById("title");
+heading.innerText="you clicked the button"
+}
+const H =function hello(){
+    console.log("hello")
+}
